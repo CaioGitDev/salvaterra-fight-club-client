f9886337ab534a389bf234968bfc9ba834cbd2b7
CaioGitDev <dev.caiorosa@gmail.com>
2023-12-27 11:22:33 +0000

    add payments datagrid columns

--- src/components/payments-datagrid/payments-datagrid.tsx
+++ src/components/payments-datagrid/payments-datagrid.tsx
@@ -9,24 +9,46 @@ import DataGrid, {
   Toolbar,
 } from 'devextreme-react/data-grid'
 import Button from 'devextreme-react/button'
-import { useCallback, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import config from 'devextreme/core/config'
 import SelectBox from 'devextreme-react/select-box'
 import { ValueChangedEvent } from 'devextreme/ui/select_box'
-import Popup from 'devextreme-react/popup'
+import Popup, { ToolbarItem } from 'devextreme-react/popup'
 import ScrollView from 'devextreme-react/scroll-view'
 import Form, { Item } from 'devextreme-react/form'
+import { datepickOptions } from '../members-datagrid/members-datagrid'
+import FetchMembersColumns from '@/data/payments-datagrid/fetch-members-columns'
+import DataSource from 'devextreme/data/data_source'
+import notify from 'devextreme/ui/notify'
+import PostMemberPaymentAsync, {
+  Payment,
+} from '@/data/payments-form/post-member-payment'
+import CustomStore from 'devextreme/data/custom_store'
+import FetchMembersPaymentsAsync from '@/data/payments-datagrid/fetch-members-payments'
+
 config({
   editorStylingMode: 'underlined',
 })
 
-type Payment = {
-  id: number
-  membershipNumber: number
+const apiUrl = 'http://localhost:3333'
+const accessToken =
+  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlMTA3YTgzNS1kYzNmLTQ5OGItYWJlZS1kOTM1NDJmYTg1YjYiLCJpYXQiOjE3MDM2MzIyNjZ9.igk_An_aVDEMav-Tg_aHorFLqJsBD44AzGmJVG4xuKSkYP_tEZ6K62ZmGJWQwRU8qiUKTU5hdk2yUtVBqopwDMZf1Rhtv_Yj4updOg6Y66HhRCzWaFQzVaYnXzetxINcaT-GPfI4IT75Aaal_NtN03yOuT813SgurTRVzxb2mxnExVAzFLfVFKG4t5XN9sttwAJ5wU1cJUuSAv-ii_3ldgF9_lAb30a1sepsmCyVE7D0_WGbPD8uVlaa8Ol6rvKBsMYg1LZDRh8EsuUQEelaloM63-WKma2bF_WbWqKOkQiiKAhVO-ZBlq-fbSTCkdpGigfUVjh3CKSYih7Vq1pH-A'
+const paymentMethod = [
+  { id: 'MBWAY', text: 'MBWAY' },
+  { id: 'MULTIBANCO', text: 'Multibanco' },
+  { id: 'TRANSFERENCIA_BANCARIA', text: 'Transferência bancária' },
+  { id: 'DINHEIRO', text: 'Dinheiro' },
+]
+
+const paymentType = [
+  { id: 'COTA_MENSAL', text: 'Cota mensal' },
+  { id: 'COTA_ANUAL', text: 'Cota anual' },
+  { id: 'SEGURO', text: 'Seguro' },
+]
+
+type MembersAutocomplete = {
+  id: string
   fullName: string
-  paymentType: string
-  amountPaid: number
-  createdAt: string
 }
 
 const paymentsColumnsDefinition = [
@@ -35,11 +57,12 @@ const paymentsColumnsDefinition = [
     visible: false,
   },
   {
-    dataField: 'membershipNumber',
+    dataField: 'member.membershipNumber',
     caption: 'Nº Socio',
+    visible: false,
   },
   {
-    dataField: 'fullName',
+    dataField: 'member.fullName',
     caption: 'Nome Completo',
   },
   {
@@ -47,12 +70,19 @@ const paymentsColumnsDefinition = [
     caption: 'Tipo de Pagamento',
   },
   {
-    dataField: 'amountPaid',
+    dataField: 'paymentMethod',
+    caption: 'Método de Pagamento',
+  },
+  {
+    dataField: 'paymentAmount',
     caption: 'Valor Pago',
+    format: '€ #,##0.##',
   },
   {
     dataField: 'createdAt',
     caption: 'Data de Pagamento',
+    dataType: 'datetime',
+    format: 'dd-MM-yyyy',
   },
 ]
 
@@ -79,54 +109,52 @@ const getCurrentMonth = () => {
   return month
 }
 
-// seed data
-const payments: Payment[] = [
-  {
-    id: 1,
-    membershipNumber: 1,
-    fullName: 'João Silva',
-    paymentType: 'Mensalidade',
-    amountPaid: 10,
-    createdAt: '2023-11-01',
-  },
-  {
-    id: 2,
-    membershipNumber: 2,
-    fullName: 'Maria Silva',
-    paymentType: 'Mensalidade',
-    amountPaid: 35,
-    createdAt: '2023-12-03',
-  },
-  {
-    id: 3,
-    membershipNumber: 3,
-    fullName: 'José Silva',
-    paymentType: 'Mensalidade',
-    amountPaid: 50.99,
-    createdAt: '2023-12-05',
-  },
-  {
-    id: 3,
-    membershipNumber: 3,
-    fullName: 'José Silva',
-    paymentType: 'Cota Anual',
-    amountPaid: 12,
-    createdAt: '2023-12-05',
-  },
-  {
-    id: 3,
-    membershipNumber: 3,
-    fullName: 'José Silva',
-    paymentType: 'Seguro',
-    amountPaid: 12,
-    createdAt: '2023-12-05',
-  },
-]
+const membersColumnsToGetAutocomplete = ['id', 'fullName']
 
 const PaymentsDatagrid = () => {
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
   const [popupVisible, setPopupVisible] = useState(false)
+  const [membersColumns, setMembersColumns] =
+    useState<DataSource<MembersAutocomplete, string>>()
+
+  // Assuming your initial state is undefined, you can set it to an empty array if you prefer
+  const [gridDataSource, setGridDataSource] =
+    useState<CustomStore<Payment[], string>>()
+
   const gridRef = useRef<DataGrid>(null)
+  const formRef = useRef<Form>(null)
+
+  useEffect(() => {
+    setGridDataSource(
+      new CustomStore<Payment[]>({
+        key: 'id',
+        load: async () => {
+          const payments = await FetchMembersPaymentsAsync({
+            apiUrl,
+            accessToken,
+          })
+
+          return payments
+        },
+      }),
+    )
+  }, [])
+
+  useEffect(() => {
+    setMembersColumns(
+      new DataSource({
+        key: 'id',
+        loadMode: 'raw',
+        load: () =>
+          FetchMembersColumns({
+            apiUrl,
+            accessToken,
+            columns: membersColumnsToGetAutocomplete,
+          }),
+        sort: [{ selector: 'fullName', desc: false }],
+      }),
+    )
+  }, [])
 
   const handleAddPaymentClick = useCallback(() => {
     setPopupVisible(true)
@@ -150,11 +178,74 @@ const PaymentsDatagrid = () => {
     gridRef.current?.instance.refresh()
   }, [])
 
+  const createPaymentAsync = useCallback(() => {
+    const formInstance = formRef.current?.instance
+
+    if (!formInstance?.validate().isValid) {
+      notify(
+        {
+          message: 'Por favor preencha todos os campos obrigatórios',
+          position: {
+            my: 'center',
+            at: 'center',
+          },
+        },
+        'error',
+        3000,
+      )
+      return
+    }
+    const { memberId, paymentType, paymentMethod, paymentAmount, paymentDate } =
+      formInstance?.option('formData')
+
+    // post payment data to api
+    const payment = PostMemberPaymentAsync(
+      {
+        apiUrl,
+        accessToken,
+      },
+      {
+        memberId,
+        paymentType,
+        paymentMethod,
+        paymentAmount,
+        paymentDate,
+      },
+    )
+
+    if (!payment) {
+      notify(
+        {
+          message: 'Ocorreu um erro ao adicionar o pagamento',
+          position: {
+            my: 'center',
+            at: 'center',
+          },
+        },
+        'error',
+        3000,
+      )
+    }
+
+    setPopupVisible(false)
+    formInstance?.option('formData', null)
+  }, [])
+
+  const getSaveButtonOptions = useCallback(
+    () => ({
+      icon: 'save',
+      stylingMode: 'outlined',
+      text: 'Adicionar pagamento',
+      onClick: createPaymentAsync,
+    }),
+    [createPaymentAsync],
+  )
+
   return (
     <div suppressHydrationWarning className="dx-viewport p-5">
       <div className={styles.right_side}></div>
       <DataGrid
-        dataSource={payments}
+        dataSource={gridDataSource}
         showBorders={false}
         height={700}
         defaultColumns={paymentsColumnsDefinition}
@@ -218,12 +309,67 @@ const PaymentsDatagrid = () => {
         width="90vw"
         height="80vh"
       >
+        <ToolbarItem
+          widget="dxButton"
+          toolbar="bottom"
+          location="after"
+          options={getSaveButtonOptions()}
+        />
         <ScrollView width="100%" height="100%">
-          <Form showValidationSummary={true}>
+          <Form ref={formRef} showValidationSummary={true} colCount={2}>
+            <Item
+              colSpan={2}
+              isRequired={true}
+              dataField="memberId"
+              editorType="dxSelectBox"
+              editorOptions={{
+                dataSource: membersColumns,
+                displayExpr: 'fullName',
+                valueExpr: 'id',
+                searchEnabled: true,
+                searchMode: 'contains',
+                searchExpr: ['fullName'],
+              }}
+              label={{ text: 'Nome Completo', location: 'top' }}
+            />
             <Item
               isRequired={true}
-              dataField="fullName"
-              label={{ text: 'Nome Completo', location: 'top' }}
+              dataField="paymentType"
+              editorType="dxSelectBox"
+              editorOptions={{
+                items: paymentType,
+                valueExpr: 'id',
+                displayExpr: 'text',
+              }}
+              label={{ text: 'Tipo de Pagamento', location: 'top' }}
+            />
+            <Item
+              isRequired={true}
+              dataField="paymentAmount"
+              editorType="dxNumberBox"
+              editorOptions={{
+                format: '€ #,##0.##',
+                value: 0,
+              }}
+              label={{ text: 'Valor Pago', location: 'top' }}
+            />
+            <Item
+              isRequired={true}
+              dataField="paymentMethod"
+              editorType="dxSelectBox"
+              editorOptions={{
+                items: paymentMethod,
+                valueExpr: 'id',
+                displayExpr: 'text',
+              }}
+              label={{ text: 'Método de Pagamento', location: 'top' }}
+            />
+            <Item
+              isRequired={true}
+              dataField="paymentDate"
+              editorType="dxDateBox"
+              editorOptions={datepickOptions}
+              label={{ text: 'Data de Pagamento', location: 'top' }}
             />
           </Form>
         </ScrollView>
