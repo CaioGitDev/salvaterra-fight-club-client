a378cb0a43b965336c8bbab7c385974345812b20
Caio Rosa <dev.caiorosa@gmail.com>
2024-07-27 12:15:27 +0100

    Update npm dependencies for braces and fill-range packages

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -3,40 +3,6 @@ import LogoImage from '../../../public/images/logo.svg'
 
 export function LoginForm() {
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="flex flex-col items-center justify-center sm:mx-auto sm:w-full sm:max-w-sm ">
-        <Image
-          className="object-cover"
-          src={LogoImage}
-          alt=""
-          width={100}
-          height={100}
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-primary-text-light">
-          Sign in to your account
-        </h2>
-      </div>
-
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <div>
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium leading-6 text-primary-text-light"
-          >
-            Email address
-          </label>
-          <div className="mt-2">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    
   )
 }
