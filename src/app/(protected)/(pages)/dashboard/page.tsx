efd7f2cd55218fb3a3edc9ce80525134bbba45db
Caio Rosa <dev.caiorosa@gmail.com>
2024-07-28 10:19:46 +0100

    refactor: Move Dashboard page to protected folder and update authentication logic

--- src/app/(protected)/(pages)/dashboard/page.tsx
+++ src/app/(protected)/(pages)/dashboard/page.tsx
@@ -1,5 +1,6 @@
-const Dashboard = () => {
-  return <div>Dashboard page</div>
-}
+import { auth } from '../../../../../auth'
 
-export default Dashboard
+export default async function Dashboard() {
+  const session = await auth()
+  return <div>{JSON.stringify(session)}</div>
+}
